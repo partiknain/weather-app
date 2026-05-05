9665933ea036ce9a3a7f4b653ded6574289e5f47
partiknain <pnain359@gmail.com>
2026-05-06 00:07:05 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('search-button');
+const Input = document.getElementById('city-input');
+const cityname =document.getElementById('city-name');
+const temp = document.getElementById('temp');
+const time = document.getElementById('city-time');
+async function getdata(cityName){
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=1bd75a6b7c404b069d8125941260505&q=${cityName}&aqi=no`);
+    return  await response.json();
+}
+button.addEventListener('click', async () => {
+    const cityName = Input.value;
+    const data = await getdata(cityName);
+    cityname.textContent =`${data.location.name}, ${data.location.country}`;
+    temp.textContent=`${data.current.temp_c}°C`;
+    time.textContent=`${data.location.localtime}`;
+})
